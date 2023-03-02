07b855f96915b659c22b54492072b16dbaa30eb6
gbrlcm <gabrielfcarvalho360@icloud.com>
2023-03-01 22:31:35 -0300

    Habitat impl

--- Sources/Habitat/Habitat.swift
+++ Sources/Habitat/Habitat.swift
@@ -1,6 +1,27 @@
-public struct Habitat {
-    public private(set) var text = "Hello, World!"
+//
+//  Habitat.swift
+//  
+//
+//  Created by Gabriel Ferreira de Carvalho on 26/09/22.
+//
 
-    public init() {
+import Foundation
+
+/// Holder of dependencias to be injected
+public struct Habitat {
+    
+    private static var current = Habitat()
+    
+    private init() {}
+    
+    public static subscript<Key> (key: Key.Type) -> Key.Value where Key: HabitatKey {
+        get { key.currentValue }
+        set { key.currentValue = newValue }
     }
+    
+    public static subscript<T> (_ keyPath: WritableKeyPath<Habitat, T>) -> T {
+        get { current[keyPath: keyPath] }
+        set { current[keyPath: keyPath] = newValue }
+    }
+    
 }
